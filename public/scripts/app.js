b92d1587e3b79d47b90045d7e60f95ca5ee8cf0b
Michael Wettstein <chischte@gmx.ch>
2020-07-04 15:43:50 +0200

    Complete chapter 10

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,8 @@
 "use strict";
 
-console.log('app.js is running!'); // JSX JacaScript XML
+console.log('app.js is running!'); // JSX JavaScript XML
 
-var template = /*#__PURE__*/React.createElement("p", null, "This is JSX from app.js");
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Indecision App"), /*#__PURE__*/React.createElement("p", null, "This is some info"));
+var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Haudi Gaudi"), /*#__PURE__*/React.createElement("p", null, "Age: 1024"), /*#__PURE__*/React.createElement("p", null, "Location: Here"));
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 console.log('app.js is running!');
 
 
-// JSX JacaScript XML
-var template = <p>This is JSX from app.js</p>
+// JSX JavaScript XML
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Haudi Gaudi</h1>
+        <p>Age: 1024</p>
+        <p>Location: Here</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app')
-ReactDOM.render(template,appRoot);
+ReactDOM.render(templateTwo, appRoot);
 
