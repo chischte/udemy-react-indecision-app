71bcdc5ac047e08b8f2eccb1aa180796dd6a8b87
Michael Wettstein <chischte@gmx.ch>
2020-07-19 14:19:50 +0200

    Complete chapter 41

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ class IndecisionApp extends React.Component {
     this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
     this.handlePickOne = this.handlePickOne.bind(this);
     this.state = {
-      options: [],
+      options: props.options,
     };
   }
   handlePickOne() {
@@ -33,12 +33,11 @@ class IndecisionApp extends React.Component {
     });
   }
   render() {
-    const title = "Indecision";
     const subtitle = "Put your life in the hands of a computer";
 
     return (
       <div>
-        <Header title={title} subtitle={subtitle} />
+        <Header subtitle={subtitle} />
         <Action
           hasOptions={this.state.options.length > 0}
           handlePickOne={this.handlePickOne}
@@ -52,16 +51,22 @@ class IndecisionApp extends React.Component {
     );
   }
 }
+IndecisionApp.defaultProps = {
+options: []
 
-const Header = (props)=>{
+};
+
+const Header = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
-      <h2>{props.subtitle}</h2>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
     </div>
   );
-}
-
+};
+Header.defaultProps = {
+  title: "Indecision",
+};
 
 const Action = (props) => {
   return (
@@ -120,13 +125,4 @@ class AddOption extends React.Component {
   }
 }
 
-// const User = (props) => {
-//   return (
-//     <div>
-//       <p>Name: {props.name} </p>
-//       <p>Age: {props.age}</p>
-//     </div>
-//   );
-// };
-
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+ReactDOM.render(<IndecisionApp/>, document.getElementById("app"));
