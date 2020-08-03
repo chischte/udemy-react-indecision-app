639de3f6f8db3d42062d665534e3da987e3ff064
Michael Wettstein <chischte@gmx.ch>
2020-08-03 18:27:20 +0200

    Fix browser warnings when using modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Modal from "react-modal";
 
-const OptionModal = (props) => (
-<Modal
-isOpen={!!props.selectedOption}
-onRequestClose={props.handleClearSelectedOption}
-contentLabel="Selected Option"
->
-<h3>Selected Option</h3>
-{props.selectedOption&& <p>{props.selectedOption}</p>}
-<button onClick={props.handleClearSelectedOption}>OK</button>
-</Modal>
-);
-export default OptionModal;
+export default class OptionModal extends React.Component {
+  componentDidMount() {
+    Modal.setAppElement("body");
+  }
+
+  render() {
+    return (
+      <Modal
+        isOpen={!!this.props.selectedOption}
+        onRequestClose={this.props.handleClearSelectedOption}
+        contentLabel="Selected Option"
+      >
+        <h3>Selected Option</h3>
+        {this.props.selectedOption && <p>{this.props.selectedOption}</p>}
+        <button onClick={this.props.handleClearSelectedOption}>OK</button>
+      </Modal>
+    );
+  }
+}
