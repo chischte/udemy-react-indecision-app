424d9f06d6063fa6c2dc73737cb399692ca0ba91
Michael Wettstein <chischte@gmx.ch>
2020-08-03 15:14:38 +0200

    BKP before transforming to babel-transform-class-properties new syntax

--- src/app.js
+++ src/app.js
@@ -3,3 +3,33 @@ import ReactDOM, { render } from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+
+class OldSyntax { // Without babel plugin "transform-class-properties"
+    constructor(){
+        this.getGreeting=this.getGreeting.bind(this);
+        this.name= 'Mike';
+    }
+    getGreeting(){
+        return `Hi. My name is ${this.name}`;
+    }
+}
+
+const oldSyntax= new OldSyntax();
+const getGreeting=oldSyntax.getGreeting;
+console.log(getGreeting());
+
+//----------
+
+class NewSyntax{
+    name= 'jen';
+    getGreeting=()=> {
+        return `Hi. My name is ${this.name}`;
+
+    }
+
+}
+
+const newSyntax=new NewSyntax;
+console.log(newSyntax);
+const newGetGreeting=newSyntax.getGreeting;
+console.log(newGetGreeting());
