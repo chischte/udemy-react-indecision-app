aa685bda139360f4007379a853ba517c078e8dd0
Michael Wettstein <chischte@gmx.ch>
2020-08-03 12:22:19 +0200

    Complete chapter 54, use webpack with babel

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('app.js is running')
+import validator from "validator";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const template = <p>testing 123</p>;
+
+ReactDOM.render(template, document.getElementById("app"));
