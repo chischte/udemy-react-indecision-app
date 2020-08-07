eb9cb137bfb2784788cbd1dbe90900c26df60535
Michael Wettstein <chischte@gmx.ch>
2020-08-07 18:59:31 +0200

    Complete chapter 71, modal styling

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -12,10 +12,12 @@ export default class OptionModal extends React.Component {
         isOpen={!!this.props.selectedOption}
         onRequestClose={this.props.handleClearSelectedOption}
         contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className="modal"
       >
-        <h3>Selected Option</h3>
-        {this.props.selectedOption && <p>{this.props.selectedOption}</p>}
-        <button onClick={this.props.handleClearSelectedOption}>OK</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {this.props.selectedOption && <p  className="modal__body">{this.props.selectedOption}</p>}
+        <button className="button" onClick={this.props.handleClearSelectedOption}>OK</button>
       </Modal>
     );
   }
